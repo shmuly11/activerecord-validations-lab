de290b035d744711471df050e33aa7089be97590
shmuly11 <shmuly11@gmail.com>
2021-01-17 17:40:25 -0500

    commit tree 27843bbba818e2a5ef51e23c9a1702991ba823e4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates :title, presence: true
+    validate :click_bait
+
+    def click_bait
+        if !title.include?("Won't Believe" || "Secret" || "Top #{Integer}" || "Guess")
+            errors.add(:title, "not clickbait")
+        end
+    end
+     
 end
